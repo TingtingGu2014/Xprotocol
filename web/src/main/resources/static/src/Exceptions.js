701fb7cb1a690fccd72e772d31a56eab820b7d6b
taozhaolib <tao.zhao.lib@gmail.com>
2017-11-26 23:34:38 -0600

    added some exception handling functions

---
+++ web/src/main/resources/static/src/Exceptions.js
@@ -0,0 +1,17 @@
+export function EmptyUserUUIDException(code, message){
+    this.message = message
+    this.code = code
+    this.name = 'EmptyUserUUIDException'
+}
+
+export function EmptyUserProtocolUUIDException(code, message){
+    this.message = message
+    this.code = code
+    this.name = 'EmptyUserProtocolUUIDException'
+}
+
+export function EmptyDataException(code, message){
+    this.message = message
+    this.code = code
+    this.name = 'EmptyDataException'
+}
