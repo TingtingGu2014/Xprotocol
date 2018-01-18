9bfecc2b7ce86a96a844ff7dc02eb0c91475b0a8
taozhaolib <tao.zhao.lib@gmail.com>
2018-01-18 00:53:07 -0600

    added protocol list and comment list on home page

--- web/src/main/resources/static/src/components/Home.vue
+++ web/src/main/resources/static/src/components/Home.vue
@@ -1,34 +1,58 @@
 <template>
-    
+    <div>
+        
+        <ProtocolList v-if="loggedIn"
+            :userUUID = "userUUID"
+        >   
+        </ProtocolList>
+        
+        <CommentList v-if="loggedIn"
+            :userUUID = "userUUID"
+        ></CommentList>
+        
+    </div>
 </template>
 
 <script>
 
+    import ProtocolList from './ProtocolList.vue'
+    import CommentList from './CommentList.vue'
+    
+    var Utils = require('../utils/Utils')
+    var loggedIn = !Utils.isEmpty(Utils.readCookie('loggedIn'))
+    var userInfo = null
+    var userUUID = ''
+
+    if(loggedIn === true) {
+        try{
+            userInfo = JSON.parse(localStorage.userInfo)
+            userUUID = userInfo.userUUID
+        }
+        catch(err) {
+            console.log(err.message)
+            localStorage.userInfo = ''
+        }
+    }
 export default {
-  name: 'index',
-  components: {
-
-  },
-  data () {
+    name: 'index',
+    data: function(){
     return {
-
+        name: 'Home',
+        userUUID: userUUID, 
+        loggedIn: loggedIn,
     }
   },
-  computed: {
-
+  components: {
+      ProtocolList, CommentList
   },
   methods: {
-    
-  },
-  mounted () {
-    
-  },
-  beforeDestroy () {
-    
+
   }
 }
 </script>
 
 <style lang="stylus">
-
+    h4 {
+        text-align: center;
+    }
 </style>
